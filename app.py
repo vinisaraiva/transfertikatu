6f5a58b55046d474946096f0153e46bb3f77e77f
Vinicius <vinisaraiva@gmail.com>
2024-01-31 14:38:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 import os
 
+# Função para autenticar e conectar ao Google Sheets
 def authenticate_google_sheets():
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
     creds = None
@@ -26,15 +27,12 @@ def authenticate_google_sheets():
     client = gspread.authorize(creds)
     return client
 
-def upload_data_to_sheet(uploaded_file, sheet_url):
+# Função para enviar dados para a planilha do Google Sheets
+def upload_data_to_sheet(data_list, sheet_url):
     client = authenticate_google_sheets()
     sheet = client.open_by_url(sheet_url)
     worksheet = sheet.get_worksheet(0)
-
-    if uploaded_file is not None:
-        data = pd.read_excel(uploaded_file, header=1)
-        data_list = data.values.tolist()
-        worksheet.append_rows(data_list)
+    worksheet.append_rows(data_list)
 
 def main():
     st.title("Upload de Arquivo Excel para Google Sheets")
@@ -42,9 +40,25 @@ def main():
     uploaded_file = st.file_uploader("Escolha um arquivo Excel", type=['xlsx', 'xls'])
     sheet_url = "https://docs.google.com/spreadsheets/d/1FPBeAXQBKy8noJ3bTF52p8JL_Eg-ptuSP6djDTsRfKE/edit#gid=0"
 
-    if st.button("Enviar para Google Sheets"):
-        upload_data_to_sheet(uploaded_file, sheet_url)
-        st.success("Dados enviados com sucesso para o Google Sheets.")
+    if uploaded_file is not None:
+        # Lê e exibe os dados da planilha Excel
+        data = pd.read_excel(uploaded_file, header=1)
+        st.write("Dados lidos do arquivo Excel:")
+        st.dataframe(data)
+        data_list = data.values.tolist()
+
+        # Botão para conectar ao Google Sheets
+        if st.button("Conectar ao Google Sheets"):
+            try:
+                authenticate_google_sheets()
+                st.success("Conectado com sucesso ao Google Sheets!")
+            except Exception as e:
+                st.error(f"Falha ao conectar: {e}")
+
+        # Botão para enviar dados para o Google Sheets
+        if st.button("Enviar para Google Sheets"):
+            upload_data_to_sheet(data_list, sheet_url)
+            st.success("Dados enviados com sucesso para o Google Sheets.")
 
 if __name__ == '__main__':
     main()
