4b0b0dcf85d4cd1f34794750121fc8a85b906f2a
Vinicius <vinisaraiva@gmail.com>
2024-01-31 17:48:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google.oauth2.service_account import Credentials
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+from streamlit_gsheets import GSheetsConnection
 import os
 
 # Escopos e ID da planilha
