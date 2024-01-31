17d800639b690f243ff8ec9e2ee23a11ada602b0
Vinicius <vinisaraiva@gmail.com>
2024-01-31 17:21:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import gspread
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from google.oauth2.service_account import Credentials
 import os
 
 # Escopos e ID da planilha
