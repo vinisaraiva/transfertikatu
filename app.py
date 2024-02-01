5ef932d8e50cb230bb38c5dc91f399d8ab66ec96
Vinicius <vinisaraiva@gmail.com>
2024-02-01 01:50:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ from google.oauth2.credentials import Credentials
 from oauth2client.service_account import ServiceAccountCredentials
 import os
 
+import streamlit as st
+import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import os
+
 def authenticate_google_sheets():
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
     creds = ServiceAccountCredentials.from_json_keyfile_name('cacesso.json', SCOPES)
@@ -17,15 +23,12 @@ def authenticate_google_sheets():
 def insert_data_to_sheet(df, sheet_url):
     client = authenticate_google_sheets()
     sheet = client.open_by_url(sheet_url)
-    worksheet = sheet.get_worksheet(0)  # Primeira aba
-    # Converte o DataFrame para uma lista de listas e insere na primeira linha vazia
+    worksheet = sheet.get_worksheet(0)
     for index, row in df.iterrows():
         worksheet.append_row(row.values.tolist())
 
 def main():
-    st.title("Upload de Arquivo Excel para Google Sheets")
-    client = None
-
+    st.title("Upload e Inserção de Arquivo Excel no Google Sheets")
     uploaded_file = st.file_uploader("Escolha um arquivo Excel", type=['xlsx', 'xls'])
     data = None
     if uploaded_file is not None:
@@ -34,17 +37,10 @@ def main():
         st.dataframe(data)
 
     sheet_url = "https://docs.google.com/spreadsheets/d/1FPBeAXQBKy8noJ3bTF52p8JL_Eg-ptuSP6djDTsRfKE/edit#gid=0"
-
-    if st.button("Conectar ao Google Sheets"):
-        client = authenticate_google_sheets()
-        if client:
-            st.success("Conectado com sucesso ao Google Sheets!")
-        else:
-            st.error("Falha ao conectar ao Google Sheets.")
-
-    if st.button("Enviar para Google Sheets") and data is not None and client is not None:
+    if st.button("Inserir Dados no Google Sheets") and data is not None:
         insert_data_to_sheet(data, sheet_url)
-        st.success("Dados enviados com sucesso para o Google Sheets.")
+        st.success("Dados inseridos com sucesso no Google Sheets.")
 
 if __name__ == '__main__':
     main()
+
