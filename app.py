7f0fd34c7349b7369676b81dd07ea11cd9a21016
Vinicius <vinisaraiva@gmail.com>
2024-02-01 00:38:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 
 def authenticate_google_sheets():
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('creden.json', SCOPES)
+    creds = ServiceAccountCredentials.from_json_keyfile_name('cacesso.json', SCOPES)
     client = gspread.authorize(creds)
     return client
 
