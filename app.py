1a567cb1f533539a0d2bee7f41f3c560c3a68c24
Vinicius <vinisaraiva@gmail.com>
2024-02-01 12:12:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,21 @@ from google.oauth2.credentials import Credentials
 from oauth2client.service_account import ServiceAccountCredentials
 import os
 
+def authenticate_google_sheets():
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('cacesso.json', SCOPES)
+    return gspread.authorize(creds)
+
+def insert_data_to_sheet(client, df, sheet_url):
+    sheet = client.open_by_url(sheet_url)
+    worksheet = sheet.get_worksheet(0)
+    data_list = df.applymap(lambda x: str(x) if pd.notnull(x) else '').values.tolist()
+    next_row = len(worksheet.get_all_values()) + 1
+    worksheet.insert_rows(data_list, next_row)
+
 def main():
     st.title("Upload e Inserção de Arquivo Excel no Google Sheets")
-    client = authenticate_google_sheets()  # Mova a autenticação para fora do botão
+    client = authenticate_google_sheets()
     uploaded_file = st.file_uploader("Escolha um arquivo Excel", type=['xlsx', 'xls'])
 
     if uploaded_file is not None:
@@ -21,22 +33,11 @@ def main():
     sheet_url = "https://docs.google.com/spreadsheets/d/1FPBeAXQBKy8noJ3bTF52p8JL_Eg-ptuSP6djDTsRfKE/edit#gid=0"
 
     if st.button("Conectar ao Google Sheets"):
-        if client:
-            st.success("Conectado com sucesso ao Google Sheets.")
-        else:
-            st.error("Falha ao conectar ao Google Sheets.")
+        st.success("Conectado com sucesso ao Google Sheets.")
 
-           if st.button("Enviar para Google Sheets") and uploaded_file is not None:
-            try:
-                if data is not None:
-                    insert_data_to_sheet(client, data, sheet_url)
-                    st.success("Dados inseridos com sucesso no Google Sheets.")
-                else:
-                    st.error("Nenhum dado para enviar. Faça o upload de um arquivo Excel primeiro.")
-            except Exception as e:
-                st.error(f"Falha ao inserir dados: {e}")
+    if st.button("Enviar para Google Sheets") and uploaded_file is not None:
+        insert_data_to_sheet(client, data, sheet_url)
+        st.success("Dados inseridos com sucesso no Google Sheets.")
 
 if __name__ == '__main__':
     main()
-
-
