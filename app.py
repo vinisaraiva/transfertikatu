119c2d65d6fc00baf4612a289843af38a46a213e
Vinicius <vinisaraiva@gmail.com>
2024-02-01 00:49:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import gspread
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -7,45 +8,34 @@ from google.oauth2.credentials import Credentials
 from oauth2client.service_account import ServiceAccountCredentials
 import os
 
-import streamlit as st
-import pandas as pd
-import numpy as np
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-
-# Criar DataFrame de dados fictícios
 def create_dummy_data():
-    data = np.random.randint(0, 100, size=(10, 24))  # 10 linhas e 24 colunas (A a X)
+    data = np.random.randint(0, 100, size=(10, 24))
     df = pd.DataFrame(data, columns=list('ABCDEFGHIJKLMNOPQRSTUVWX'))
     return df
 
-# Função para autenticar e conectar ao Google Sheets
 def authenticate_google_sheets():
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('cacesso.json', SCOPES)
+    creds = ServiceAccountCredentials.from_json_keyfile_name('creden.json', SCOPES)
     client = gspread.authorize(creds)
     return client
 
-# Função para inserir dados do DataFrame na planilha do Google Sheets
 def insert_data_to_sheet(df, sheet_url):
     client = authenticate_google_sheets()
     sheet = client.open_by_url(sheet_url)
-    worksheet = sheet.get_worksheet(0)  # assumindo que queremos inserir na primeira aba
-    # Converte o DataFrame para uma lista de listas e insere na planilha
-    worksheet.update('A1', df.values.tolist())
+    worksheet = sheet.get_worksheet(0)
+    for index, row in df.iterrows():
+        worksheet.append_row(row.values.tolist())
 
 def main():
     st.title("Inserção de Dados Fictícios no Google Sheets")
-
     df_dummy = create_dummy_data()
     st.write("Dados fictícios gerados:")
     st.dataframe(df_dummy)
-
     sheet_url = "https://docs.google.com/spreadsheets/d/1FPBeAXQBKy8noJ3bTF52p8JL_Eg-ptuSP6djDTsRfKE/edit#gid=0"
-
     if st.button("Inserir Dados no Google Sheets"):
         insert_data_to_sheet(df_dummy, sheet_url)
         st.success("Dados inseridos com sucesso no Google Sheets.")
 
 if __name__ == '__main__':
     main()
+
