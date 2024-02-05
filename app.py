683c03c10e6cb73a66f685824ccc121dd61af4e6
Vinicius <vinisaraiva@gmail.com>
2024-02-05 11:09:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def main():
                 #root > div:nth-child(1) > div > div > div > div > section > div {padding-bottom: 0rem;}
         </style>
         """, unsafe_allow_html=True)
-    st.subheader(":blue[&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Envio dos dados coletados]")
+    st.subheader(":blue[&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Envio dos dados coletados]")
 
     uploaded_file = st.file_uploader("SELECIONE ABAIXO O ARQUIVO", type=['xlsx', 'xls'])
     client, drive_service = authenticate_google_services()
