cc24b8a0b0b455def3df119ccf9096273284850c
Vinicius <vinisaraiva@gmail.com>
2024-02-05 10:23:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -41,63 +41,24 @@ def upload_file_to_drive(drive_service, filename, filepath, folder_id):
     return file.get('id')
 
 def main():
-    imagem = Image.open('bannerapp1.png')
-    st.image (imagem, caption='')
-
-    st.title(":blue[App para envio de dados do monitoramento da água]")
-
-     # CSS para personalizar os botões
-    button_style = """
+    st.title("Upload e Inserção de Arquivo Excel no Google Sheets e Backup no Google Drive")
+    
+    st.markdown("""
     <style>
     .stButton>button {
         color: white;
         background-color: #084d6e;
-        font-size: 18px;
+        font-size: 16px;
         font-weight: bold;
     }
     </style>
-    """
-    st.markdown(button_style, unsafe_allow_html=True)
-
-    
-    st.markdown("""
-    <style>
-        .reportview-container {
-            margin-top: -2em;
-        }
-        #MainMenu {visibility: hidden;}
-        .stDeployButton {display:none;}
-        footer {visibility: hidden;}
-        header {visibility: hidden;}
-        #stDecoration {display:none;}
-    </style>
     """, unsafe_allow_html=True)
 
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    st.markdown("""
-        <style>
-               .block-container {
-                    padding-top: 0.3rem;
-                    padding-bottom: 0rem;
-                    padding-left: 0.3rem;
-                    padding-right: 0.3rem;
-    
-                }
-                
-                
-        </style>
-        """, unsafe_allow_html=True)
+    uploaded_file = st.file_uploader("Escolha um arquivo Excel", type=['xlsx', 'xls'])
+    client, drive_service = None, None
 
-    client, drive_service = authenticate_google_services()
-
-    uploaded_file = st.file_uploader("<b>Escolha um arquivo Excel</b>", type=['xlsx', 'xls'])
-
-    if uploaded_file is not None:
-        # Salva o arquivo temporariamente
-        with open("temp_file.xlsx", "wb") as f:
-            f.write(uploaded_file.getbuffer())
-        data = pd.read_excel("temp_file.xlsx", header=0)
-        # Ordena e filtra pelo valor mais recente
+    if uploaded_file:
+        data = pd.read_excel(uploaded_file, header=0)
         data['Date'] = pd.to_datetime(data['Date'])
         data.sort_values(by='Date', ascending=False, inplace=True)
         most_recent_date = data['Date'].max()
@@ -105,24 +66,28 @@ def main():
         st.write("Dados filtrados do arquivo Excel para a data mais recente:")
         st.dataframe(filtered_data)
 
-    sheet_url = "https://docs.google.com/spreadsheets/d/1FPBeAXQBKy8noJ3bTF52p8JL_Eg-ptuSP6djDTsRfKE/edit#gid=0"
-    folder_id = '14-m18deS8QWYsSGSMFHq3Iyy_vPzQmUa'  # ID da pasta no Google Drive
-
-    if st.button("Conectar ao Google Sheets", key='connect_google_sheets'):
-        st.success("Conectado com sucesso ao Google Sheets.")
-
-    if st.button("Enviar para Google Sheets e fazer backup no Drive", key='send_to_google_sheets_drive') and uploaded_file is not None:
-        if not filtered_data.empty:
-            insert_data_to_sheet(client, filtered_data, sheet_url)
+    cols = st.columns(2)
+    with cols[0]:
+        if st.button("Conectar ao Google Sheets", key='connect_google_sheets'):
+            client, drive_service = authenticate_google_services()
+            if client:
+                st.success("Conectado com sucesso ao Google Sheets.")
+            else:
+                st.error("Falha ao conectar ao Google Sheets.")
+    
+    with cols[1]:
+        if st.button("Enviar para Google Sheets", key='send_to_google_sheets') and uploaded_file and client:
+            insert_data_to_sheet(client, filtered_data, sheet_url="https://docs.google.com/spreadsheets/d/1FPBeAXQBKy8noJ3bTF52p8JL_Eg-ptuSP6djDTsRfKE/edit#gid=0")
+            st.success("Dados da data mais recente inseridos com sucesso no Google Sheets.")
+    
+    if uploaded_file and client and drive_service:
+        folder_id = '14-m18deS8QWYsSGSMFHq3Iyy_vPzQmUa'
+        if st.button("Fazer backup no Drive", key='backup_to_drive'):
             file_id = upload_file_to_drive(drive_service, uploaded_file.name, "temp_file.xlsx", folder_id)
-            st.success(f"Dados da data mais recente inseridos com sucesso no Google Sheets e backup realizado no Google Drive. ID do arquivo: {file_id}")
-        else:
-            st.error("Não há dados da data mais recente para enviar.")
+            st.success(f"Backup realizado no Google Drive. ID do arquivo: {file_id}")
 
 if __name__ == '__main__':
     main()
-
-
 
 
 
